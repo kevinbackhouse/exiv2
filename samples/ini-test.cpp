8123f931f30cf13b95c2df77320c2e4aafc6b134
Kevin Backhouse <kevinbackhouse@github.com>
2022-12-07 23:40:37 +0000

    Work in progress.

--- samples/ini-test.cpp
+++ samples/ini-test.cpp
@@ -9,6 +9,7 @@ Config loaded from : 'initest.ini' version=6, name=Bob Smith, email=bob@smith.co
 */
 
 // Example that shows simple usage of the INIReader class
+#include <INIReader.h>
 #include <exiv2/exiv2.hpp>
 #include <iostream>
 
